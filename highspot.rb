4ba02347cf4af3cd5a0a37b66f1505b476a7fb94
kate myer <kate.d.mangubat@gmail.com>
2020-06-04 17:57:47 -0700

    highspot coding problem solution

--- highspot.rb
+++ highspot.rb
@@ -1,32 +1,41 @@
+# Question: Given two strings a and b and your task will be to return the characters that are not common in the two strings
 
-a = "soss"
-b = "sogt"
+a = "dogsaaaa"
+# b = "dogtaaab"
+b = "aaabdogcats"
 
-#output = "st"
+#Time = O(n + m) looping through a_chars array and checking against values of b_chars array and vice versa
+#space = O(n) as length of strings grow, possibly, the char array holder can grow. 
+
 def not_match(a,b)
-  char = []
+  not_common = []
   a_chars = a.split("")
   b_chars = b.split("")
 
   puts "a #{a_chars}"
   puts "b #{b_chars}"
 
-  i = 0
-  while i < a_chars.length
+  #loop through a chars
 
-    #check
-    if a_chars[i] != b_chars[i] # d-d
-      char.push(a_chars[i])
-      char.push(b_chars[i])
+  a_chars.each do |char|
+    puts "a char #{char}"
+    #check if char from a_char included in b_char
+    if !b_chars.include?(char)
+      not_common.push(char)
     end
-    i += 1
   end
 
-  char = char.join
-  return char
+  b_chars.each do |char|
+    puts "b char #{char}"
+    #check if char from b_char included in a_char
+    if !a_chars.include?(char)
+      not_common.push(char)
+    end
+  end
+
+  not_common = not_common.join
+  return not_common
 end
 
 p not_match(a,b)
 
-#time = O(n) looping through a_chars array and checking against values of b_chars array
-#space = O(n) as length of strings grow, possibly, the char array holder can grow. 
